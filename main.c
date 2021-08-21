79cb3087e040f530edab6faa679db83ffbb6e307
rohantehalyani <rohan.tehalyani11@gmail.com>
2021-08-21 13:30:34 +0530

    func

--- main.c
+++ main.c
@@ -3,7 +3,26 @@
 
 int main(){
     
+/*
+    Read function
     char *filePath;
-    filePath = malloc(256);  
+    filePath = malloc(256); 
+    filePath[255] = '\0'; 
+    scanf("%255s", filePath);//take input of file name
+    //check it valid file name is given 
+    while(access(filePath,R_OK) != 0){
+    printf("\nUnable to open the file !!!!\nPlease check th
+    printf("Write the name of file as \"File Name\".csv : "
+    scanf("%255s", filePath);
+    }
+
     FILE *file_p = fopen(filePath,"r");
-}
+
+    int *cls = readcls(file_p)
+    //int tradeNo = 1, buyp = 0, sellp = 0, pl = 0, totalpl = 0;
+
+    free(filePath);
+    */
+   
+    return 0;
+    }
