101d6d7d92bc3c40922b3522ec7bced9acb3335c
rohantehalyani <rohan.tehalyani11@gmail.com>
2021-08-21 15:20:32 +0530

    earn profit

---
+++ 3_Implementation/src/earning.c
@@ -0,0 +1,7 @@
+#include<header.h>
+
+float earning(int consumed_unit, int rate, int time_consumed)
+{
+    float earn= consumed_unit*rate;
+    return earn;
+}
---
+++ 3_Implementation/src/profit.c
@@ -0,0 +1,7 @@
+#include<header.h>
+
+float profit(float earn,float paidtogrid)
+{
+    float profit=earn-paidtogrid;
+    return profit;
+}
