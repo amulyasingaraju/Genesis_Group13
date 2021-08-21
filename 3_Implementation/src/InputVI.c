e103f6f539160947adc0a0fd5e7a5ef1c4d53b42
AbhidhaChoudhari <86190226+AbhidhaChoudhari@users.noreply.github.com>
2021-08-21 15:54:52 +0530

    Add files via upload
    input.c added

---
+++ 3_Implementation/src/InputVI.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int AdminInput()
+{
+    int voltage;
+    int current;
+    printf(" Enter Voltage requirement:   "); 
+    scanf(" %d", &voltage);
+    printf(" Enter Current requirement:   "); 
+    scanf(" %d", &current);
+    printf(" The amount of voltage and current required by EV is: Voltage = %d V, Current = %d Amp", voltage ,current);
+    return voltage,current;
+}
+
+int main()
+    {
+AdminInput();
+
+    }
