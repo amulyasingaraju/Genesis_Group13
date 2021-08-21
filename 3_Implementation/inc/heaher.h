103f6be97d1b0cfa8e540b0b317756cfeb93775e
rohantehalyani <rohan.tehalyani11@gmail.com>
2021-08-21 12:41:33 +0530

    function

---
+++ 3_Implementation/inc/heaher.h
@@ -0,0 +1,9 @@
+#ifndef __READCSV_H
+#define __READCSV_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+int *readcls(FILE *file_p);
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <header.h>
+#include <unistd.h>
+
+int main(){
+    
+    char *filePath;
+    filePath = malloc(256);  
+    FILE *file_p = fopen(filePath,"r");
+}
