d28202095a52125efdfd05d073c4aa296bb0ff45
rohantehalyani <rohan.tehalyani11@gmail.com>
2021-08-21 17:54:33 +0530

    portpercost

--- 3_Implementation/src/earning.c
+++ 3_Implementation/src/earning.c
@@ -1,6 +1,6 @@
 #include<header.h>
 
-float earning(int consumed_unit, int rate, int time_consumed)
+float earning(int consumed_unit, int rate, float time_consumed)
 {
     float earn= consumed_unit*rate;
     return earn;
---
+++ 3_Implementation/src/portcost.c
@@ -0,0 +1,6 @@
+float portpercost(int consumed_unit, int port_rate, float time_consumed){
+    for(int i=0;i<5;i++)
+    {
+        
+    }    
+}
