9a3f39b6e30bff54c15ce18c6161e0218265f7c8
DEEPAK KUMAR CHOUDHARY <deepakkumarchy929@gmail.com>
2021-08-24 01:14:20 +0530

    folder structure added

--- 3_Implementation/inc/header.h
+++ 3_Implementation/inc/header.h
@@ -1,11 +1,21 @@
-#ifndef __READCSV_H
-#define __READCSV_H
+#ifdef _HEADER_H
+#define _HEADER_H
+#include<stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<time.h>
+#include<ctype.h>
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
-
-int *readcsv(FILE *file_p);
+void welcomeMessage(void);
+void init(void);
+login();
+int user_input(void);
+int port_input(void);
+int power_compare(int,int,int);
+int islanded_mode(int,int,int);
+int grid_mode(int,int,int);
+int earnedbyev(int);
+int totalprofit(int,int);
 
 #endif
