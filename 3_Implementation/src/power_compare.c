31169a81973917bc6359c15adbea1b1aaac346d0
Anirudh11706436 <62830313+Anirudh11706436@users.noreply.github.com>
2021-08-23 19:28:35 +0530

    Create power_compare.c

---
+++ 3_Implementation/src/power_compare.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include"header.h"
+
+int grid_power = 110000; // 110 kWh
+
+int power_compare(int pv_power,int load_demand,int PROFITGRID)
+{
+    if(pv_power >= load_demand)
+    {
+        islanded_mode(pv_power,load_demand,PROFITGRID);
+    }
+    else
+    {
+        grid_mode(pv_power,load_demand,PROFITGRID);
+    }
+}
