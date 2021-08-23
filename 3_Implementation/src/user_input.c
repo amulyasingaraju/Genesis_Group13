70080b71ceb99fb61ac8f01adf79eb4db6a71888
pottikoteswararao <86213638+pottikoteswararao@users.noreply.github.com>
2021-08-23 18:07:29 +0530

    Update user_input.c

--- 3_Implementation/src/user_input.c
+++ 3_Implementation/src/user_input.c
@@ -86,4 +86,67 @@ printf("\t\t\t\t\t**** Customer Screen ****\n\n");
                 wait(2);
                 printf("Payment Sucessfull.\n\n");
                 break;
-                
+            case 2:
+                printf("waiting\n\n");
+                wait(2);
+                printf("Payment Sucessfull.\n");
+                break;
+            default:
+                printf("please select either 1 or 2\n");
+         }
+         printf("|||||---------Thank you.--------|||||\n\n");
+         // time required to charge one scooter per unit is 2 minutes
+         int wait_time = (amount_s/5) *2;
+         printf("Estimated Waiting Time %d Minutes.\n\n",wait_time);
+         printf("Charging Completed....\nThank You, we will be happy to serve you again.\n\n");
+
+         load_demand+= delivered_power;
+         PROFITGRID+= amount_s;
+     }  
+ }
+
+ for(int i=3; i<5; i++)
+ {  // to calculate pwoer required to charge car 
+     if(port[i]==1)
+     {
+         printf("Dear Customer currently you are using port %d.\n\n",i+1);
+         int amount_c;
+         printf("Please Enter Amount(in multiple of 5) :");
+         scanf("%d",&amount_c);
+         printf("\n\n");
+         int delivered_power = (amount_c/5) * 1000; // to store energy in Wh  
+         printf("Please select payment mode.\n\n1. UPI\n2. Card\n");
+         //printf("1. UPI\n2. Card\n");
+         scanf("%d",&select);
+         switch(select)
+         {
+            case 1:
+                printf("waiting\n\n");
+                wait(2);
+                printf("Payment Sucessfull.\n\n");
+                break;
+            case 2:
+                printf("waiting\n\n");
+                wait(2);
+                printf("Payment Sucessfull.\n");
+                break;
+            default:
+                printf("please select either 1 or 2\n");
+         }
+         printf("|||||---------Thank you.--------|||||\n\n");
+
+         // time required to charge one scooter per unit is 3 minutes
+         int wait_time = (amount_c/5) *3;
+         printf("Estimated Waiting Time %d Minutes.\n\n",wait_time);
+         printf("Charging Completed....\n Thank You, we will be happy to serve you again.\n");
+
+         load_demand+= delivered_power;
+         PROFITGRID+= amount_c;  
+     }  
+ }
+ printf("\t\t\t\t\t**** Owner Screen ****\n\n");
+ printf("Current Load demand is %d Wh.\n\n",load_demand);
+ 
+ power_compare(pv_power,load_demand,PROFITGRID);
+ //return load_demand;
+}
